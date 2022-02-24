600b5b28e7a33f2d14fa0e8591c220b40a6b831b
Prosper Oyibo <1606628@csl-student.wlv.ac.uk>
2022-02-24 12:31:55 +0000

    First commit

---
+++ Models/NewsModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class NewsModel extends Model
+{
+    protected $table = 'news';
+	protected $allowedFields = ['title', 'slug', 'body'];
+	
+	// This returns news items from database
+		public function getNews($slug = false)
+		
+	{
+		// If no slug (Id) from new db is provided then select all
+		if ($slug === false) {
+			return $this->findAll();
+		}
+		
+        // if slug (Id) is provided then select that one
+		return $this->where(['slug' => $slug])->first();
+	}
+}

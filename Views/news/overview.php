780ab3d434b82a13509ac8e8197084c1c4b57d47
Prosper Oyibo <1606628@csl-student.wlv.ac.uk>
2022-03-31 10:51:08 +0100

    Added a carousel after page title

--- Views/news/overview.php
+++ Views/news/overview.php
@@ -1,5 +1,28 @@
 
-<a class="btn btn-secondary mb-4" href="<?=base_url()?>/news/create">Create article</a>
+<a class="btn btn-secondary mb-4" href="<?=base_url()?>/news/create">Create article</a> <br/>
+
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="<?=base_url('images/book-shelf.jpg')?>" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="<?=base_url('images/book-shelf.jpg')?>" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="<?=base_url('images/book-shelf.jpg')?>" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div> <br/>
+
 
 <?php if (! empty($news) && is_array($news)): ?>
 

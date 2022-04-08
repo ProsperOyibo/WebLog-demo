6f4a539df2be7ff2a26ff85daba3a88d782c217d
Prosper Oyibo <1606628@csl-student.wlv.ac.uk>
2022-04-08 13:09:19 +0100

    Final commit for project

--- Views/pages/about.php
+++ Views/pages/about.php
@@ -7,7 +7,7 @@
          <img src="<?=base_url('images/avatar.webp')?>" alt="Someone looking through a Microscope"  width="330" height="320" style="float: left; border-radius: 50%; margin-right: 50px; margin-left: 30px; margin-top: 50px;">
          <h2>About</h2>
          <p>This webpage is designed to give you a brief insight into different types of books. It gives you general information on what a pacticular book
-is all about. 		 </p>
+is all about. The webpage can also act as a list for users to personally customize, aswell as set goals on how many books they want to read	in a given month/year.	 </p>
   </section>
   
   
